aca3e8493827073205c37fee334c559394799f33
fyardlest <fyardlest1@gmail.com>
2025-12-08 13:04:48 -0500

    Render build script

--- build.sh
+++ build.sh
@@ -5,7 +5,3 @@ pip install -r requirements.txt
 python manage.py migrate --no-input
 python manage.py collectstatic --no-input
 
-
-chmod +x build.sh
-git add build.sh && git commit -m "Add Render build script"
-git push
